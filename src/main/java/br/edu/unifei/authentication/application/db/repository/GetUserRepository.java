299958529d7723553b0e4ad89922d5bf8c3410fb
Leonardo F Oliveira <leonfoliveira@gmail.com>
2022-05-10 19:33:45 -0300

    add find method to repository interfaces

--- src/main/java/br/edu/unifei/authentication/application/db/repository/GetUserRepository.java
+++ src/main/java/br/edu/unifei/authentication/application/db/repository/GetUserRepository.java
@@ -3,7 +3,13 @@ package br.edu.unifei.authentication.application.db.repository;
 import br.edu.unifei.authentication.domain.entity.User;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface GetUserRepository {
     List<User> getAll();
+
+    Optional<User> findById(UUID id);
+
+    Optional<User> findByLogin(String login);
 }
--- src/main/java/br/edu/unifei/transaction/application/db/repository/GetSaleRepository.java
+++ src/main/java/br/edu/unifei/transaction/application/db/repository/GetSaleRepository.java
@@ -3,7 +3,11 @@ package br.edu.unifei.transaction.application.db.repository;
 import br.edu.unifei.transaction.domain.entity.Sale;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface GetSaleRepository {
     List<Sale> getAll();
+
+    Optional<Sale> findById(UUID id);
 }
