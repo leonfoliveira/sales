79d82448500e29126486a1dab46375072a0052c8
Leonardo F Oliveira <leonfoliveira@gmail.com>
2022-05-13 19:20:50 -0300

    add DbUpdateProductUsecase

--- src/test/java/br/edu/unifei/inventory/application/dto/UpdateProductDTOMock.java
+++ src/test/java/br/edu/unifei/inventory/application/dto/UpdateProductDTOMock.java
@@ -3,10 +3,13 @@ package br.edu.unifei.inventory.application.dto;
 import br.edu.unifei.inventory.domain.entity.UnitType;
 import com.github.javafaker.Faker;
 
-public abstract class CreateProductDTOMock {
-    public static CreateProductDTO get() {
+import java.util.UUID;
+
+public abstract class UpdateProductDTOMock {
+    public static UpdateProductDTO get() {
         Faker faker = new Faker();
-        return new CreateProductDTO(
+        return new UpdateProductDTO(
+                UUID.randomUUID(),
                 faker.lorem().sentence(),
                 faker.number().digits(10),
                 faker.random().nextDouble(),
