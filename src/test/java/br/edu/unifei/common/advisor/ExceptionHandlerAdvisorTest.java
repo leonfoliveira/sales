9c2fb28de43a844532d1778b869ab26e1ad1c8eb
Leonardo F Oliveira <leonfoliveira@gmail.com>
2022-05-14 18:43:13 -0300

    add ExceptionHandlerAdvisorTest

---
+++ src/test/java/br/edu/unifei/common/advisor/ExceptionHandlerAdvisorTest.java
@@ -0,0 +1,59 @@
+package br.edu.unifei.common.advisor;
+
+import br.edu.unifei.common.exception.*;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExceptionHandlerAdvisorTest {
+    ExceptionHandlerAdvisor sut = new ExceptionHandlerAdvisor();
+    Faker faker = new Faker();
+
+    @Test
+    void shouldHandleConflictExceptionCorrectly() {
+        String message = faker.lorem().sentence();
+        ConflictException ex = new ConflictException(message);
+        ExceptionResponse response = sut.handle(ex);
+        assertEquals(response, new ExceptionResponse(ex.getMessage()));
+    }
+
+    @Test
+    void shouldHandleForbiddenExceptionCorrectly() {
+        String message = faker.lorem().sentence();
+        ForbiddenException ex = new ForbiddenException(message);
+        ExceptionResponse response = sut.handle(ex);
+        assertEquals(response, new ExceptionResponse(ex.getMessage()));
+    }
+
+    @Test
+    void shouldHandleNotFoundExceptionCorrectly() {
+        String message = faker.lorem().sentence();
+        NotFoundException ex = new NotFoundException(message);
+        ExceptionResponse response = sut.handle(ex);
+        assertEquals(response, new ExceptionResponse(ex.getMessage()));
+    }
+
+    @Test
+    void shouldHandleUnauthorizedExceptionCorrectly() {
+        String message = faker.lorem().sentence();
+        UnauthorizedException ex = new UnauthorizedException(message);
+        ExceptionResponse response = sut.handle(ex);
+        assertEquals(response, new ExceptionResponse(ex.getMessage()));
+    }
+
+    @Test
+    void shouldHandleBusinessRuleExceptionCorrectly() {
+        String message = faker.lorem().sentence();
+        BusinessRuleException ex = new BusinessRuleException(message);
+        ExceptionResponse response = sut.handle(ex);
+        assertEquals(response, new ExceptionResponse(ex.getMessage()));
+    }
+
+    @Test
+    void shouldHandleAnyOtherExceptionCorrectly() {
+        Exception ex = new Exception();
+        ExceptionResponse response = sut.handle(ex);
+        assertEquals(response, new ExceptionResponse("Internal server error"));
+    }
+}
