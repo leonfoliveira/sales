f92ce3dc02517c17e3973fc56dc4f539c381906f
Leonardo F Oliveira <leonfoliveira@gmail.com>
2022-05-14 18:30:15 -0300

    add GetAllUserController

---
+++ src/test/java/br/edu/unifei/authentication/presentation/springweb/controller/GetAllUserControllerTest.java
@@ -0,0 +1,41 @@
+package br.edu.unifei.authentication.presentation.springweb.controller;
+
+import br.edu.unifei.authentication.application.contract.GetAllUserUsecase;
+import br.edu.unifei.authentication.application.contract.GetAllUserUsecaseSpy;
+import br.edu.unifei.authentication.domain.entity.User;
+import br.edu.unifei.authentication.domain.entity.UserMock;
+import br.edu.unifei.authentication.presentation.springweb.response.UserResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class GetAllUserControllerTest {
+    GetAllUserUsecase getAllUserUsecaseSpy;
+    GetAllUserController sut;
+
+    @BeforeEach
+    void setup() {
+        getAllUserUsecaseSpy = GetAllUserUsecaseSpy.get();
+        sut = new GetAllUserController(getAllUserUsecaseSpy);
+    }
+
+    @Test
+    void shouldCallGetAllUserUsecase() {
+        sut.handle();
+        verify(getAllUserUsecaseSpy).handle();
+    }
+
+    @Test
+    void shouldReturnAListOfUserResponseOnSuccess() {
+        User user = UserMock.get();
+        when(getAllUserUsecaseSpy.handle())
+                .thenReturn(List.of(user));
+        List<UserResponse> response = sut.handle();
+        assertEquals(response, List.of(new UserResponse(user)));
+    }
+}
