2865965cd2d3d106b227b20e3c24b6e8472902e7
eduardo0401 <eduardoaugusto.eo@gmail.com>
2022-05-22 10:46:32 -0300

    add CreateSaleDTOMock and CreateSaleItemDTOMock

---
+++ src/test/java/br/edu/unifei/transaction/application/dto/CreateSaleItemDTOMock.java
@@ -0,0 +1,16 @@
+package br.edu.unifei.transaction.application.dto;
+
+import com.github.javafaker.Faker;
+
+import java.util.UUID;
+
+public class CreateSaleItemDTOMock {
+    public static CreateSaleItemDTO get() {
+        Faker faker = new Faker();
+        return new CreateSaleItemDTO(
+                UUID.randomUUID(),
+                Double.valueOf(faker.random().nextInt(1,15)),
+                faker.random().nextDouble()
+        );
+    }
+}
