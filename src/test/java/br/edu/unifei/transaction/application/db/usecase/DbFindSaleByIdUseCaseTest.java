2c1d5f854335a797462aade786bb9555d4bf5212
eduardo0401 <eduardoaugusto.eo@gmail.com>
2022-05-24 22:16:07 -0300

    change DbFindSaleByIdUseCaseTest class name

--- src/test/java/br/edu/unifei/transaction/application/db/usecase/DbFindSaleByIdUseCaseTest.java
+++ src/test/java/br/edu/unifei/transaction/application/db/usecase/DbFindSaleByIdUseCaseTest.java
@@ -17,7 +17,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class DbFindSaleByIDUseCaseTest {
+public class DbFindSaleByIdUseCaseTest {
     GetSaleRepository getSaleRepositorySpy;
     DbFindSaleByIdUseCase sut;
 
