b37bf2b63897a65163fcf3a43a2151a51e5d4f91
eduardo0401 <eduardoaugusto.eo@gmail.com>
2022-05-27 23:35:15 -0300

    refactor JpaSale

--- src/main/java/br/edu/unifei/transaction/persistence/jpa/entity/JpaSale.java
+++ src/main/java/br/edu/unifei/transaction/persistence/jpa/entity/JpaSale.java
@@ -1,21 +1,24 @@
 package br.edu.unifei.transaction.persistence.jpa.entity;
 
 import br.edu.unifei.transaction.domain.entity.Sale;
-import br.edu.unifei.transaction.domain.entity.SaleItem;
-import br.edu.unifei.transaction.persistence.jpa.repository.JpaSaleItemRepository;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Data
+@Table(name = "tb_sale")
 public class JpaSale {
     @Id
     private UUID id;
+    @OneToMany
+    private List<JpaSaleItem> jpaSaleItem;
     private LocalDateTime createdAt;
     private LocalDateTime deletedAt;
 
@@ -24,13 +27,20 @@ public class JpaSale {
 
     public JpaSale (Sale sale) {
         this.id = sale.getId();
+        this.jpaSaleItem = sale.getItems()
+                .stream()
+                .map(item -> new JpaSaleItem(item))
+                .toList();
         this.createdAt = sale.getCreatedAt();
     }
 
-    public Sale toDomainEntity(List<SaleItem> items) {
+    public Sale toDomainEntity() {
         return Sale.builder()
                 .id(this.id)
-                .items(items)
+                .items(jpaSaleItem
+                        .stream()
+                        .map(JpaSaleItem::toDomainEntity)
+                        .toList())
                 .createdAt(this.createdAt)
                 .build();
     }
